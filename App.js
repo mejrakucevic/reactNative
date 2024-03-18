a907129e63549610191c14f7a85d132b2de147d7
mejrakucevic <mejrakucevic420@gmail.com>
2024-03-18 23:53:02 +0100

    .

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My first app!</Text>
+    <SafeAreaView style={styles.container}>
+      {/* header */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Hello, World!</Text>
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.body}>
+        <Text>This is some text woo!</Text>
+        <Text>This is some text woo!</Text>
+        <Text>This is some text woo!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,6 +24,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  header: {
+    backgroundColor: "whitesmoke",
+    padding: 20,
+    alignSelf: "stretch",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "blue",
+    padding: 30,
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
